8ca3c8efb9c76800a45d46b3d1c7222fae3aad42
jpherrmann3 <james_herrmann3@outlook.com>
2024-05-03 16:20:40 -0700

    Added comments

--- aggregator/src/main/java/com/example/aggregator/service/AggregatorService.java
+++ aggregator/src/main/java/com/example/aggregator/service/AggregatorService.java
@@ -69,29 +69,47 @@ public class AggregatorService {
     }
 
     public List<Entry> getAllPalindromesV2() {
+
         final List<Entry> palindromes = new ArrayList<>();
+
+        // Iterate from a to z
         for(char character = 'a'; character <= 'z'; character++) {
+
+            // Change the char to string for end point
             String letter = Character.toString(character);
+
+            // Get all entries starting with the letter
             List<Entry> entires = restClient.getWordsStartingWith(letter);
+
+            // Iterate over the entries
             for(Entry entry: entires) {
+
+                // Check the letters of the word starting with the first and last index positions
                 String word = entry.getWord();
                 int startIndex = 0;
                 int endIndex = word.length() - 1;
+
+                // Loop until the start and end position either match or pass each other.
+                // If the characters at the startIndex and endIndex position match, keep going.
                 while(startIndex < endIndex) {
-                    if (word.charAt(startIndex) != word.charAt(endIndex)) {
-                        break;
-                    }
-                    else {
+                    if (word.charAt(startIndex) == word.charAt(endIndex)) {
                         startIndex++;
                         endIndex--;
                     }
+                    else {
+                        break;
+                    }
                 }
+
+                // If the start index is >= the end index, then we went through the whole word without
+                // breaking. This would be a palindrome. Add it to the array.
                 if (startIndex >= endIndex) {
                     palindromes.add(entry);
                 }
             }
         }
 
+        // sort before returning using the natural order to compare.
         palindromes.sort(Comparator.naturalOrder());
         return palindromes;
     }
