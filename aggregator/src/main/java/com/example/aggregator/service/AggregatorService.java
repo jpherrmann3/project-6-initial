b417d6510740bf86c2bc1e26471e73f4d7daf9ac
jpherrmann3 <james_herrmann3@outlook.com>
2024-05-03 16:06:56 -0700

    removed unused import

--- aggregator/src/main/java/com/example/aggregator/service/AggregatorService.java
+++ aggregator/src/main/java/com/example/aggregator/service/AggregatorService.java
@@ -3,7 +3,6 @@ package com.example.aggregator.service;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
